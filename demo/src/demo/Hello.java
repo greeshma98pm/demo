a1a18f63349a5ea4f7f91b199894a7557d9695ae
grees <grees@192.168.1.111>
2023-08-01 07:39:59 +0530

    first commit

---
+++ demo/src/demo/Hello.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Hello {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("HELLO WORLD!!");
+		Hello.main();
+	}
+
+	static void main()
+	{
+		System.out.println("main 2");
+	}
+}
---
+++ demo/src/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		HelloWorld.hello("HelloWorld");
+
+	}
+	static void hello(String name)
+	{
+		System.out.println(name);
+	}
+
+}
